a27c2a9719116c6dd80dc4d2131dac7c9fd5809e
DaoSon <daolamsoncntt@gmail.com>
2018-12-15 12:04:43 +0700

    ok

--- src/pages/Dashboard/Map.js
+++ src/pages/Dashboard/Map.js
@@ -5,7 +5,7 @@ import styles from './Map.less';
 
 var timerHandle = null;
 
-var car = "M17.402,0H5.643C2.526,0,0,3.467,0,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759c3.116,0,5.644-2.527,5.644-5.644 V6.584C23.044,3.467,20.518,0,17.402,0z M22.057,14.188v11.665l-2.729,0.351v-4.806L22.057,14.188z M20.625,10.773 c-1.016,3.9-2.219,8.51-2.219,8.51H4.638l-2.222-8.51C2.417,10.773,11.3,7.755,20.625,10.773z M3.748,21.713v4.492l-2.73-0.349 V14.502L3.748,21.713z M1.018,37.938V27.579l2.73,0.343v8.196L1.018,37.938z M2.575,40.882l2.218-3.336h13.771l2.219,3.336H2.575z M19.328,35.805v-7.872l2.729-0.355v10.048L19.328,35.805z";
+var car = "M17.402,0H5.643C2.526,0,0,3.467,0,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759c3.116,0,5.644-2.527,5.644-5.644 V6.584C23.044,3.467,20.518,0,17.402,0z M22.057,14.188v11.665l-2.729,0.351v-4.806L22.057,14.188z M22.057,14.188v11.665l-2.729,0.351v-4.806L22.057,14.188z M20.625,10.773 c-1.016,3.9-2.219,8.51-2.219,8.51H4.638l-2.222-8.51C2.417,10.773,11.3,7.755,20.625,10.773z M20.625,10.773 c-1.016,3.9-2.219,8.51-2.219,8.51H4.638l-2.222-8.51C2.417,10.773,11.3,7.755,20.625,10.773z M3.748,21.713v4.492l-2.73-0.349 V14.502L3.748,21.713z M3.748,21.713v4.492l-2.73-0.349 V14.502L3.748,21.713z M1.018,37.938V27.579l2.73,0.343v8.196L1.018,37.938z M1.018,37.938V27.579l2.73,0.343v8.196L1.018,37.938z M2.575,40.882l2.218-3.336h13.771l2.219,3.336H2.575z M2.575,40.882l2.218-3.336h13.771l2.219,3.336H2.575z M19.328,35.805v-7.872l2.729-0.355v10.048L19.328,35.805z M19.328,35.805v-7.872l2.729-0.355v10.048L19.328,35.805z";
 
 class MapPage extends PureComponent {
 
@@ -18,7 +18,7 @@ class MapPage extends PureComponent {
     poly2: null,
     eol: 0,
     icon: null,
-    step: 100,
+    step: 20,
     tick: 100,
     lastVertex: 1,
 
@@ -53,24 +53,24 @@ class MapPage extends PureComponent {
 
     this.state.polyline = new google.maps.Polyline({
       path: [],
-      strokeColor: '#FF0000',
+      strokeColor: '#f1f907',
       strokeWeight: 3
     });
 
     this.state.poly2 = new google.maps.Polyline({
       path: [],
-      strokeColor: '#FF0000',
+      strokeColor: '#f1f907',
       strokeWeight: 3
     });
 
     this.state.icon = {
       path: car,
-      scale: .7,
-      strokeColor: 'white',
+      scale: .6,
+      strokeColor: '#fff',
       strokeWeight: .10,
       fillOpacity: 1,
       fillColor: '#404040',
-      offset: '5%',
+      offset: '10%',
       anchor: new google.maps.Point(10, 25) // orig 10,50 back of car, 10,0 front of car, 10,25 center of car
     };
   }
@@ -194,7 +194,9 @@ class MapPage extends PureComponent {
     let currentPoint = this.state.marker.getPosition();
     let newPoint = this.getPointAtDistance(this.state.polyline, distance);
 
-    this.state.map.setCenter(newPoint);
+    // Set camera theo oto
+    //this.state.map.setCenter(newPoint);
+
     this.state.marker.setPosition(newPoint);
 
     let heading = google.maps.geometry.spherical.computeHeading(currentPoint, newPoint);
